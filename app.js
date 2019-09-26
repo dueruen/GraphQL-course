fdba8df7829cefbd46b3db3e63ec7d0f990e8a56
dueruen <32197042+dueruen@users.noreply.github.com>
2019-09-26 10:07:30 +0900

    ch02 static data

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const { ApolloServer, gql } = require('apollo-server-express');
+const users = require('./data').users;
+const cars = require('./data').cars;
+const me = users[0];
+
+const typeDefs = gql`
+    type Query {
+        users: [User]
+        user(id: Int!): User
+        me: User
+
+        cars: [Car]
+        car(id: Int!): Car
+    }
+
+    type User {
+        id: ID!
+        name: String!
+    }
+
+    type Car {
+        id: ID!
+        make: String!
+        model: String!
+        color: String!
+    }
+`;
+const resolvers = {
+    Query: {
+        users: () => users,
+        user: (parent, { id }) => {
+            const user = users.filter(user => user.id === id);
+            return user[0];
+        }, 
+        me: () => me,
+        cars: () => cars,
+        car: (parent, { id }) => {
+            const car = cars.filter(car => car.id === id);
+            return car[0];
+        }
+    }
+};
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+server.applyMiddleware({ app });
+
+app.listen(3000, () => console.info('Apollo GraphQL server is running on port 3000'));
---
+++ data.js
@@ -0,0 +1,32 @@
+let users = [{
+    id: 1,
+    name: 'Tamas'
+},{
+    id: 2,
+    name: 'Susan'
+},{
+    id: 3,
+    name: 'Steven '
+}];
+
+let cars = [{
+    id: 1,
+    make: 'Ford',
+    model: 'Focus',
+    color: 'red'
+},{
+    id: 2,
+    make: 'Fiat',
+    model: '500',
+    color: 'blue'
+},{
+    id: 1,
+    make: 'Mercedes',
+    model: 'C250',
+    color: 'silver'
+}]
+
+module.exports = {
+    users,
+    cars
+};
